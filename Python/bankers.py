105b28e56cc52e6d9aae9ece335a6c14c4a00b7c
nothingrealm <shafaghi1378@gmail.com>
2019-12-05 03:15:44 +0330

    bankers finished

--- Python/bankers.py
+++ Python/bankers.py
@@ -10,10 +10,10 @@ def request(max):
     return np.matrix(hold)
 
 
-def release(i, req, avail, alloc, need):
-    avail += req + alloc[i]
+def release(i, avail, alloc, need):
+    avail += alloc[i]
     need.pop(i)
-    return np.delete(alloc, 0, axis=0)
+    return np.delete(alloc, i, axis=0)
 
 
 def check(x, y):
@@ -33,7 +33,7 @@ def safe(avail, need, alloc):
         need_shadow = need.copy()
         # alloc_shadow = alloc.copy()
         avail_shadow = avail.copy()
-        alloc_shadow = release(i, np.zeros([1, 10], dtype=np.int32)[0], avail_shadow, alloc, need_shadow)
+        alloc_shadow = release(i, avail_shadow, alloc, need_shadow)
         if safe(avail_shadow, need_shadow, alloc_shadow):
             return True
     return False
@@ -42,7 +42,7 @@ def safe(avail, need, alloc):
 if __name__ == '__main__':
     flag = True
     res = []
-    avail = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    avail = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
     # avail = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     max = []
     alloc = []
@@ -69,27 +69,47 @@ if __name__ == '__main__':
         hold += alloc[i]
 
     res = hold + avail
+
+    print('resources are:')
     print(res)
+    print('available resourcesr are:')
+    print(avail)
+    print('needs are:')
+    print(need)
+    print('allocated resources are:')
+    print(alloc)
+
+    # --------------------------------
+
     while flag:
-        preq = random.randint(0, len(alloc) - 1)
-        requested = request(need[preq])
+        req_id = random.randint(0, len(alloc) - 1)
+        requested = request(need[req_id])
         need_shadow = need.copy()
         avail_shadow = np.array(avail - requested)
+        can = True
         for i in avail_shadow[0]:
             if i < 0:
-                print("row " + str(preq) + " requested " + str(
-                    requested) + " and failed because lack of available resource")
-                continue
-        need_shadow[preq] = np.array(need_shadow[preq] - requested)[0]
+                print("row " + str(req_id) + " requested " + str(
+                    requested) + " and failed because requested resources were greater than available resources ")
+                can = False
+                break
+        if not can:
+            continue
+        need_shadow[req_id] = np.array(need_shadow[req_id] - requested)[0]
+        alloc_shadow = alloc.copy()
+        alloc_shadow[req_id] = np.array(requested + alloc_shadow[req_id])[0]
         zf = True
-        for i in need_shadow[preq]:
+        for i in need_shadow[req_id]:
             if i != 0:
                 zf = False
                 break
         if zf:
-            print("row " + str(preq) + " requested " + str(
+            print("row " + str(req_id) + " requested " + str(
                 requested) + "and now is done.")
-            alloc = release(preq, requested, avail, alloc, need)
+            alloc = alloc_shadow
+            need = need_shadow
+            avail = avail_shadow
+            alloc = release(req_id, avail, alloc, need)
             print("allocates now are:")
             print(alloc)
             print("needs now are:")
@@ -99,17 +119,16 @@ if __name__ == '__main__':
             if len(alloc) == 0:
                 flag = False
             continue
-        alloc_shadow = alloc.copy()
-        alloc_shadow[preq] = np.array(requested + alloc_shadow[preq])[0]
         state = safe(avail_shadow[0], need_shadow, alloc_shadow)
         if not state:
-            print("row " + str(preq) + " requested " + str(requested) + " and deadlock happened so we skip the request")
+            print("row " + str(req_id) + " requested " + str(
+                requested) + " and deadlock happened so we skip the request")
         else:
-            print("row " + str(preq) + " requested " + str(requested) + " and we are in safe state")
+            print("row " + str(req_id) + " requested " + str(requested) + " and we are in safe state")
             need = need_shadow
             avail = avail_shadow
             alloc = alloc_shadow
             print("availables now are:")
             print(avail)
 
-    print("All are done without deadlock")
+    print("All are done without deadlock (now available resources are equal with resources)")
