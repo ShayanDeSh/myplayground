aa4f4dce4b0a9c4622e90f4db2462bb6493ce49b
nothingrealm <shafaghi1378@gmail.com>
2019-12-04 22:46:05 +0330

    working on safe method

--- Python/bankers.py
+++ Python/bankers.py
@@ -2,8 +2,6 @@ import threading
 import random
 import numpy as np
 
-flag = True
-
 
 def request(i):
     hold = []
@@ -12,64 +10,74 @@ def request(i):
     return np.matrix(hold)
 
 
-def release(i, req):
+def release(i, req, avail, alloc):
     avail += req + alloc[i]
     alloc.pop(i)
 
 
-def safe(avail, need):
-    for i in range(10):
-        test = need[i] - avail[i]
-
 def check(x, y):
-    flag = True
     for i in range(10):
         if x[i] > y[i]:
             flag = False
-            return False
     return True
-res = []
-avail = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-max = []
-alloc = []
-need = []
-for i in range(10):
-    hold = []
-    for j in range(10):
-        hold.append(random.randint(0, 10))
-    max.append(hold)
-for i in range(10):
-    hold = []
-    for j in range(10):
-        hold.append(random.randint(0, max[i][j]))
-    alloc.append(hold)
 
-maxMatrix = np.matrix(max)
-allocMatrix = np.matrix(alloc)
 
-for i in range(10):
-    need.append(maxMatrix[i] - allocMatrix[i])
-hold = np.zeros([1, 10])
-# print(alloc)
-for i in range(10):
-    hold += alloc[i]
+def safe(avail_shadow, shadow_need, alloc_shadow):
+    for i in range(10):
+        flag = check(shadow_need, avail_shadow)
+        if not flag:
+            continue
+        release(i, np.zeros([1, 10]), avail_shadow, alloc_shadow)
 
-res = hold + avail
-print(res)
-while flag:
-    preq = random.randint(0, 10)
-    requested = request(preq)
-    shadow = need.copy()
-    test = avail - requested
-    for i in test:
-        if i < 0:
-            print(preq + "requested " + requested " and failed")
-    zf = True
-    for i in test:
-        if i != 0:
-            zf = False
-    if zf:
-        release(preq, requested)
 
-    shadow[preq] = shadow[preq] - requested
-    safe(test, shadow)
+
+if __name__ == '__main__':
+    flag = True
+    res = []
+    avail = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    max = []
+    alloc = []
+    need = []
+    for i in range(10):
+        hold = []
+        for j in range(10):
+            hold.append(random.randint(0, 10))
+        max.append(hold)
+    for i in range(10):
+        hold = []
+        for j in range(10):
+            hold.append(random.randint(0, max[i][j]))
+        alloc.append(hold)
+
+    maxMatrix = np.matrix(max)
+    allocMatrix = np.matrix(alloc)
+
+    for i in range(10):
+        need.append(maxMatrix[i] - allocMatrix[i])
+    hold = np.zeros([1, 10])
+    # print(alloc)
+    for i in range(10):
+        hold += alloc[i]
+
+    res = hold + avail
+    print(res)
+    while flag:
+        preq = random.randint(0, 10)
+        requested = request(preq)
+        need_shadow = need.copy()
+        avail_shadow = avail - requested
+        for i in avail_shadow:
+            if i < 0:
+                print(preq + " requested " + requested + " and failed because lack of available resource")
+                continue
+        zf = True
+        for i in avail_shadow:
+            if i != 0:
+                zf = False
+        if zf:
+            print(preq + " is done")
+            release(preq, requested, avail, alloc)
+            continue
+        need_shadow[preq] = need_shadow[preq] - requested
+        alloc_shadow = alloc.copy()
+        safe(avail_shadow, need_shadow, alloc_shadow)
