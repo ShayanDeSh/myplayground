373584ea32d7654cc6059e404d1d2a85d210d84c
nothingrealm <shafaghi1378@gmail.com>
2019-12-17 19:53:38 +0330

    Async Deep Dive start

---
+++ C#/AsyncDeepDive/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace AsyncLearning {
+    class Program {
+        static void Main(string[] args) {
+            FooAsync2();
+            Console.ReadLine();
+        }
+
+        static async void FooAsync() {
+        }
+
+        static void FooAsync2() {
+            var stateMachine = new FooAsyncStateMachine();
+
+            stateMachine.MethodBuilder = new AsyncVoidMethodBuilder();
+            
+            stateMachine.MethodBuilder.Start(ref stateMachine);
+        }
+
+        struct FooAsyncStateMachine : IAsyncStateMachine {
+            public AsyncVoidMethodBuilder MethodBuilder;
+
+            public void MoveNext() {
+                // State machine
+                MethodBuilder.SetResult();
+            }
+
+            public void SetStateMachine(IAsyncStateMachine stateMachine) {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
