e2c05a13d7be52eb8c36fa27ce6b9f526e47692a
nothingrealm <shafaghi1378@gmail.com>
2019-12-18 10:40:37 +0330

    Openmp

---
+++ C/PIserie.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+	static long num_steps = 100000;
+	double step;
+	int i;
+	double x, pi, sum = 0.0;
+	step = 1.0/(double) num_steps;
+	for(i = 0; i < num_steps; i++) 
+	{
+		x = (i + 0.5) * step;
+		sum = sum + 4.0/(1.0 + x * x);
+	}
+	pi = step * sum;
+	printf("pi =  %lf\n", pi);
+}
