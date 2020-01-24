a0cf861e479c7bc427ac8d2b18c6515609423dce
nothingrealm <shafaghi1378@gmail.com>
2020-01-24 21:52:46 +0330

    A liitle enhancement

--- Matlab/SpectralSubtraction/spectral_subtraction.m
+++ Matlab/SpectralSubtraction/spectral_subtraction.m
@@ -1,18 +1,18 @@
 [s, fs] = audioread('Test.wav');
 plot(s);
-s = awgn(s,20,'measured');
+s = awgn(s,10,'measured');
 figure
 plot(s)
-number_of_samples = 0.02 * fs;
-overlap = 1 / 2;
+number_of_samples = 0.03 * fs;
+overlap = 1 / 6;
 offset = number_of_samples * overlap;
 n = length(s);
 result = zeros(n, 1);
 noise_est = 0;
 sound(s, fs);
 pause(4)
-sample_noise = 48;
-ftn = 340;
+sample_noise = 40;
+ftn = 512;
 for i=1:offset:sample_noise * offset
     fi = i + number_of_samples - 1;
     if fi > n
@@ -49,7 +49,7 @@ for i=1:offset:n
     complex_spec = fft(windowed_frame,ftn);        % use a 512 point fft
     mag_spec = abs(complex_spec);
     phase_spec = angle(complex_spec);
-    clean_spec = mag_spec - 2 * noise_est;
+    clean_spec = mag_spec - 3 * noise_est;
     clean_spec(clean_spec < 0) = 0;
     enh_spec = clean_spec.*exp(1j*phase_spec);
     enh_spec = real(ifft(enh_spec, length(frame)));
