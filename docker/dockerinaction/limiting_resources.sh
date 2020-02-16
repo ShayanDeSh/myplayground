e7ac2977d8e620bbe1bcf4f672426ba4b205d563
NothingRealm <shafaghi1378@gmail.com>
2020-02-16 17:59:40 -0500

    Alocating a cpu set to a container.

--- docker/dockerinaction/limiting_resources.sh
+++ docker/dockerinaction/limiting_resources.sh
@@ -11,7 +11,15 @@ docker stats ch6_mariadb
 
 docker container run -d -P --name ch6_wordpress --memory 512m \
     --cpu-shares 512 \
+#   --cpus 0.75 \
     --cap-drop net_raw \
     --link ch6_mariadb:mysql \
     -e WORDPRESS_DB_PASSWORD=test \
     wordpress:5.0.0-php7.2-apache
+
+
+docker container run -d \
+    --cpuset-cpus 0 \
+    --name ch6_stresser dockerinaction/ch6_stresser
+
+docker container run -it --rm dockerinaction/ch6_htop
