28feff5873f5860bef81a94c07e34e8ace67db27
nothingrealm <shafaghi1378@gmail.com>
2020-02-23 14:10:26 +0330

    Added readonly option for the configuration file of nginx

--- docker/dockerinaction/bind_mount.sh
+++ docker/dockerinaction/bind_mount.sh
@@ -3,7 +3,7 @@ CONF_DST=/etc/nginx/conf.d/default.conf
 LOG_SRC=~/example.log
 LOG_DST=/var/log/nginx/custom.host.access.log
 docker run -d --name diaweb \
-    --mount type=bind,src=${CONF_SRC},dst=${CONF_DST} \
+    --mount type=bind,src=${CONF_SRC},dst=${CONF_DST},readonly=true \
     --mount type=bind,src=${LOG_SRC},dst=${LOG_DST} \
     -p 80:80 \
     nginx:latest
