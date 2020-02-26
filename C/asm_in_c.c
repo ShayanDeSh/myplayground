fcd7bf95b50ab82990caee3e829dd2c2640fe489
NothingRealm <shafaghi1378@gmail.com>
2020-02-26 05:16:24 -0500

    Using mov in asm

---
+++ C/asm_in_c.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+void main() 
+{
+    int x = 1;
+    int y = 2;
+
+    asm ("mov %0, %1"
+            : "=r" (y)
+            : "r" (x)
+            );
+
+    printf("y = %d, x = %d \n", y, x);
+
+}
