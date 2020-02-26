396f6670ede7c9bf4fca56e4b26f5852470acd36
NothingRealm <shafaghi1378@gmail.com>
2020-02-26 06:07:08 -0500

    Specifying the register

--- C/asm_in_c.c
+++ C/asm_in_c.c
@@ -7,21 +7,28 @@ void main()
 
     asm ("mov %0, %1"
             : "=r" (y)
-            : "r" (x)
+            : "r"  (x)
             );
 
     printf("y = %d, x = %d \n", y, x);
 
     asm ("leal (%1, %1, 4), %0"
             : "=r" (y)
-            : "r" (x)
+            : "r"  (x)
         );
 
     printf("y = %d, x = %d \n", y, x);
 
     asm ("leal (%0, %0, 4), %0"
             : "=r" (x)
-            : "0" (x)
+            : "0"  (x)
+        );
+
+    printf("y = %d, x = %d \n", y, x);
+
+    asm ("leal (%%ecx, %%ecx, 4), %%ecx"
+            : "=c" (x)
+            : "c"  (x)
         );
 
     printf("y = %d, x = %d \n", y, x);
