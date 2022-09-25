f8f2a0c2b0335bd40e565504756f98dcdbedeb4c
shayandesh <shafaghi1378@gmail.com>
2022-09-25 23:47:52 +0330

    Forward udp

--- transparent_proxy/redsocks_iptables.sh
+++ transparent_proxy/redsocks_iptables.sh
@@ -11,7 +11,10 @@ iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
 iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
 iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
+iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 10053
 iptables -t nat -A OUTPUT -p tcp --dport 443 -j REDSOCKS
 iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDSOCKS
+iptables -t nat -A OUTPUT -p tcp --dport 53 -j REDSOCKS
+iptables -t nat -A OUTPUT -p udp --dport 53 -j REDSOCKS
 iptables-save > ./iptables_rules
 iptables-restore < ./iptables_rules
--- transparent_proxy/redsocks_iptables_rm.sh
+++ transparent_proxy/redsocks_iptables_rm.sh
@@ -10,8 +10,11 @@ iptables -t nat -D REDSOCKS -d 192.168.0.0/16 -j RETURN
 iptables -t nat -D REDSOCKS -d 224.0.0.0/4 -j RETURN
 iptables -t nat -D REDSOCKS -d 240.0.0.0/4 -j RETURN
 iptables -t nat -D REDSOCKS -p tcp -j REDIRECT --to-ports 12345
+iptables -t nat -D REDSOCKS -p udp -j REDIRECT --to-ports 10053
 iptables -t nat -D OUTPUT -p tcp --dport 443 -j REDSOCKS
 iptables -t nat -D OUTPUT -p tcp --dport 80 -j REDSOCKS
+iptables -t nat -A OUTPUT -p tcp --dport 53 -j REDSOCKS
+iptables -t nat -A OUTPUT -p udp --dport 53 -j REDSOCKS
 iptables -t nat -X REDSOCKS
 iptables-save > ./iptables_rules
 iptables-restore < ./iptables_rules
